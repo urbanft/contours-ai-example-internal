2461c9ee939dee338da3c9fc8b416c78f1c1f78b
Faraz Ahmad <faraz.ahmad@urbanft.com>
2022-12-20 11:22:27 +0530

    iOS and Android example projects added

---
+++ iOS/ContoursAIExampleiOS/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ContoursAIExampleiOS
+//
+//  Created by UrbanFT on 19/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
