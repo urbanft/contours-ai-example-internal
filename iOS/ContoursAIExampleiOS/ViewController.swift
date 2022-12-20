758d4cc48b2f1968bbe5eaa6f85aff4b8ba7c48e
Faraz Ahmad <faraz.ahmad@urbanft.com>
2022-12-20 12:20:58 +0530

    Usage section added for iOS

--- iOS/ContoursAIExampleiOS/ViewController.swift
+++ iOS/ContoursAIExampleiOS/ViewController.swift
@@ -6,11 +6,28 @@
 //
 
 import UIKit
+import ContoursAI_SDK
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CheckCaptureDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        openContoursAISDK()
+    }
+    
+    func openContoursAISDK() {
+      let contoursSDK = ContoursAIFramework()
+      let controller = contoursSDK.getInitialScreen(checkCapturingSide: .front, clientId: "<YOUR CLIENT ID>", captureType: CaptureType.both.rawValue, controller: self)
+      self.navigationController?.pushViewController(controller, animated: false)
+    }
+    
+    func imageCaptured(frontImageCropped: UIImage?, rearImageCropped: UIImage?, frontImage: UIImage?, rearImage: UIImage?) {
+        if frontImage != nil {
+            //Your code goes here
+        }
+        if rearImage != nil {
+            //Your code goes here
+        }
     }
 }
 
