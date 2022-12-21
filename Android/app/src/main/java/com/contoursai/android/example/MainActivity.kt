8a591c6b13b39f54c74d82899504ee54d8475e9b
Faraz Ahmad <faraz.ahmad@urbanft.com>
2022-12-21 18:39:16 +0530

    Updated Android repo with new approach

--- Android/app/src/main/java/com/contoursai/android/example/MainActivity.kt
+++ Android/app/src/main/java/com/contoursai/android/example/MainActivity.kt
@@ -5,10 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import com.urbanft.android.contours_ai.ContoursStarterActivity
-import com.urbanft.android.contours_ai.constants.ContoursConstants
-import com.urbanft.android.contours_ai.models.ContoursCapturingMode
-import com.urbanft.android.contours_ai.models.ContoursModel
 import java.net.URI
 import java.net.URISyntaxException
 
@@ -24,28 +20,21 @@ class MainActivity : AppCompatActivity() {
         contoursModel.capturingMode = ContoursCapturingMode.BOTH_CAPTURE
         contoursModel.checkFace = ContoursConstants.FRONT_FACE
         contoursModel.fontFamily = "Poppins"
-        ContoursStarterActivity.launchSdk(this, contoursStarterActivityResultLauncher, contoursModel,  "<YOUR CLIENT ID>")
-    }
-
-
-    var contoursStarterActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
-        if (result.resultCode == RESULT_OK) {
-            if (result.data == null) {
-                return@registerForActivityResult
+        ContoursStarterActivity.launchSdk(this, contoursModel, clientId, object: IContoursResultListener{
+            override fun onCaptureSuccess(contoursResultModel: ContoursResultModel) {
+                if (contoursResultModel.resultCheckFace.equals(ContoursConstants.FRONT_FACE, ignoreCase = true)) {
+                    val frontCroppedImageUri = contoursResultModel.resultFrontCroppedImageUri
+                    val rearCroppedImageUri = contoursResultModel.resultRearCroppedImageUri
+                    val frontImageUri = contoursResultModel.resultFrontImageUri
+                    val rearImageUri = contoursResultModel.resultRearImageUri
+                    showCapturedImage(<IMAGE_URI>)
+                } else if (contoursResultModel.resultCheckFace.equals(ContoursConstants.BACK_FACE, ignoreCase = true)) {
+                    val rearCroppedImageUri = contoursResultModel.resultRearCroppedImageUri
+                    val rearImageUri = contoursResultModel.resultRearImageUri
+                    showCapturedImage(<IMAGE_URI>)
+                }
             }
-            val face = result.data!!.getStringExtra(ContoursConstants.RESULT_FACE_KEY)
-            if (face.equals(ContoursConstants.FRONT_FACE, ignoreCase = true)) {
-                val frontCroppedImageUri = result.data?.getStringExtra(ContoursConstants.RESULT_FRONT_CROPPED_IMAGE_URI)
-                val rearCroppedImageUri = result.data?.getStringExtra(ContoursConstants.RESULT_REAR_CROPPED_IMAGE_URI)
-                val frontImageUri = result.data?.getStringExtra(ContoursConstants.RESULT_FRONT_IMAGE_URI)
-                val rearImageUri = result.data?.getStringExtra(ContoursConstants.RESULT_REAR_IMAGE_URI)
-                showCapturedImage(<IMAGE_URI>)
-            } else if (face.equals(ContoursConstants.BACK_FACE, ignoreCase = true)) {
-                val rearCroppedImageUri = result.data?.getStringExtra(ContoursConstants.RESULT_REAR_CROPPED_IMAGE_URI)
-                val rearImageUri = result.data?.getStringExtra(ContoursConstants.RESULT_REAR_IMAGE_URI)
-                showCapturedImage(<IMAGE_URI>)
-            }
-        }
+        })
     }
 
     private fun showCapturedImage(imageUri: String?) {
