5f89ed2bcb2d47ce5624a399f9d8b81b3ef9fec5
Ajay Kumar <ajay.kumar@urbanft.com>
2023-04-04 19:56:47 +0530

    Android example updated for Contour AI SDK 1.2.0

--- Android/app/src/main/java/com/contoursai/android/example/MainActivity.kt
+++ Android/app/src/main/java/com/contoursai/android/example/MainActivity.kt
@@ -1,37 +1,80 @@
 package com.contoursai.android.example
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.contourdocumentimaging.android.contours_ai.ContoursStarterActivity
+import com.contourdocumentimaging.android.contours_ai.callback.IContoursResultListener
+import com.contourdocumentimaging.android.contours_ai.constants.ContoursConstants
+import com.contourdocumentimaging.android.contours_ai.models.ContoursCapturingMode
+import com.contourdocumentimaging.android.contours_ai.models.ContoursModel
+import com.contourdocumentimaging.android.contours_ai.models.ContoursResultModel
 import java.net.URI
 import java.net.URISyntaxException
 
 class MainActivity : AppCompatActivity() {
+
+    private var ivFront: ImageView? = null
+    private var ivBack: ImageView? = null
+    private var tvFront: TextView? = null
+    private var tvBack: TextView? = null
+    private var bitmapFront: Bitmap? = null
+    private var bitmapBack: Bitmap? = null
+    private var imageName: String? = null
+
+    // Check face controls the capture of front or back check face.
+    // It's important to set it correctly because that affects different
+    // stages and thresholds of metrics for the process.
+    private var checkFace = ContoursConstants.FRONT_FACE
+    private val clientId = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvFront = findViewById<View>(R.id.tvFront) as TextView
+        tvBack = findViewById<View>(R.id.tvBack) as TextView
+        ivFront = findViewById<View>(R.id.ivFront) as ImageView
+        ivBack = findViewById<View>(R.id.ivBack) as ImageView
+        ivFront!!.setOnClickListener { openContours(true) }
+        ivBack!!.setOnClickListener { openContours(false) }
+    }
+
+    /**
+     *
+     * @param isFront
+     */
+    private fun openContours(isFront: Boolean) {
+        checkFace = if (isFront) {
+            ContoursConstants.FRONT_FACE
+        } else {
+            ContoursConstants.BACK_FACE
+        }
         takePicture()
     }
 
     private fun takePicture() {
         val contoursModel = ContoursModel()
         contoursModel.capturingMode = ContoursCapturingMode.BOTH_CAPTURE
-        contoursModel.checkFace = ContoursConstants.FRONT_FACE
+        contoursModel.checkFace = checkFace
         contoursModel.fontFamily = "Poppins"
-        ContoursStarterActivity.launchSdk(this, contoursModel, clientId, object: IContoursResultListener{
+        ContoursStarterActivity.launchSdk(this, contoursModel, clientId, object:
+            IContoursResultListener {
             override fun onCaptureSuccess(contoursResultModel: ContoursResultModel) {
                 if (contoursResultModel.resultCheckFace.equals(ContoursConstants.FRONT_FACE, ignoreCase = true)) {
-                    val frontCroppedImageUri = contoursResultModel.resultFrontCroppedImageUri
+                    /*val frontCroppedImageUri = contoursResultModel.resultFrontCroppedImageUri
                     val rearCroppedImageUri = contoursResultModel.resultRearCroppedImageUri
                     val frontImageUri = contoursResultModel.resultFrontImageUri
-                    val rearImageUri = contoursResultModel.resultRearImageUri
-                    showCapturedImage(<IMAGE_URI>)
+                    val rearImageUri = contoursResultModel.resultRearImageUri*/
+                    showCapturedImage(contoursResultModel.resultFrontCroppedImageUri, ivFront, tvFront, true)
+                    showCapturedImage(contoursResultModel.resultRearCroppedImageUri, ivBack, tvBack, false)
                 } else if (contoursResultModel.resultCheckFace.equals(ContoursConstants.BACK_FACE, ignoreCase = true)) {
-                    val rearCroppedImageUri = contoursResultModel.resultRearCroppedImageUri
-                    val rearImageUri = contoursResultModel.resultRearImageUri
-                    showCapturedImage(<IMAGE_URI>)
+                    /*val rearCroppedImageUri = contoursResultModel.resultRearCroppedImageUri
+                    val rearImageUri = contoursResultModel.resultRearImageUri*/
+                    showCapturedImage(contoursResultModel.resultRearCroppedImageUri, ivBack, tvBack, false)
                 }
             }
 
@@ -41,16 +84,22 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun showCapturedImage(imageUri: String?) {
+    private fun showCapturedImage(imageUri: String?, imageView: ImageView?, textView: TextView?, isFront: Boolean) {
         if (imageUri == null) {
             return
         }
         try {
-            val uri = URI(imageUri)
+            val backURI = URI(imageUri)
             val options = BitmapFactory.Options()
-            val bmp = BitmapFactory.decodeFile(uri.path, options)
+            val bmp = BitmapFactory.decodeFile(backURI.path, options)
             if (bmp != null) {
-                //Set bitmap on the ImageView
+                if (isFront) {
+                    bitmapFront = bmp
+                } else {
+                    bitmapBack = bmp
+                }
+                imageView!!.setImageBitmap(bmp)
+                textView!!.visibility = View.GONE
             }
         } catch (e: URISyntaxException) {
             e.printStackTrace()
