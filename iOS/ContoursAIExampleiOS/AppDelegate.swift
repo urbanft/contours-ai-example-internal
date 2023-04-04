0bcab109da09847c02d995d8a3318e980ecab4d4
Kapil Teotia <kapil.teotia@urbanft.com>
2023-04-04 12:26:32 +0530

    UIKit sample updated.

--- iOS/ContoursAIExampleiOS/AppDelegate.swift
+++ iOS/ContoursAIExampleiOS/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var isLandscape: Bool = false
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -31,6 +31,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return  isLandscape ? .landscapeRight : .portrait
+    }
 }
 
--- iOS/ContoursAIExampleiOS/ViewController.swift
+++ iOS/ContoursAIExampleiOS/ViewController.swift
@@ -8,30 +8,64 @@
 import UIKit
 import ContoursAI_SDK
 
-class ViewController: UIViewController, CheckCaptureDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        openContoursAISDK()
+class ViewController: UIViewController,CheckCaptureDelegate{
+    
+    @IBOutlet var frontImageView : UIImageView!
+    @IBOutlet var backImageView : UIImageView!
+    @IBOutlet var frontImagebutton : UIButton!
+    @IBOutlet var backImagebutton : UIButton!
+    
+    let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    let contoursSDK = ContoursAIFramework()
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    @IBAction func confirmButtonClicked(_ sender: Any) {
+        let button =  sender as? UIButton
+        openContoursSDKConcept(checkSide: button?.tag ?? 0)  //Function to  open Contours SDK
     }
     
-    func openContoursAISDK() {
-      let contoursSDK = ContoursAIFramework()
-      let controller = contoursSDK.getInitialScreen(checkCapturingSide: .front, clientId: "<YOUR CLIENT ID>", captureType: CaptureType.both.rawValue, controller: self)
-      self.navigationController?.pushViewController(controller, animated: false)
+    func openContoursSDKConcept(checkSide:Int) {
+        appDelegate?.isLandscape = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: {
+            self.view.layoutSubviews()
+            switch checkSide {
+            case 101:
+                self.openFrontOfCheck()
+            case 102:
+                self.openRearOfCheck()
+            default : break
+            }
+        })
+    }
+    
+    func openFrontOfCheck(){
+        let imageVC = contoursSDK.initializeSDK(checkCapturingSide:.front ,clientId: "<YOUR CLIENT ID>", captureType: CaptureType.both.rawValue, delegate: self)
+        self.navigationController?.pushViewController(imageVC, animated: false)
+    }
+    
+    func openRearOfCheck() {
+        let imageVC = contoursSDK.initializeSDK(checkCapturingSide: .back, clientId: "<YOUR CLIENT ID>", captureType: CaptureType.both.rawValue, delegate: self)
+        self.navigationController?.pushViewController(imageVC, animated: false)
     }
     
     func imageCaptured(frontImageCropped: UIImage?, rearImageCropped: UIImage?, frontImage: UIImage?, rearImage: UIImage?) {
         if frontImage != nil {
-            //Your code goes here
+            frontImageView.image = frontImageCropped
         }
         if rearImage != nil {
-            //Your code goes here
+            backImageView.image = rearImageCropped
         }
+        appDelegate?.isLandscape = false
+    }
+    
+    func onContourClose() {
+        appDelegate?.isLandscape = false
     }
     
     func eventCaptured(data: [String : Any]?) {
-        print(data)
     }
 }
-
