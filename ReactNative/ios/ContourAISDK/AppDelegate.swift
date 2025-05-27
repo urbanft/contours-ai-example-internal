e28187effedee4da56a831e576268d0382c1621f
Vivek Kumar Sahani <vsahani@uft-int.com>
2025-05-27 21:53:23 +0530

    [CAS-4329] React native version upgraded to v79

--- ReactNative/ios/ContourAISDK/AppDelegate.swift
+++ ReactNative/ios/ContourAISDK/AppDelegate.swift
@@ -5,17 +5,33 @@ import ReactAppDependencyProvider
 import ContoursAI_SDK
 
 @main
-class AppDelegate: RCTAppDelegate {
-  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+  
+  var reactNativeDelegate: ReactNativeDelegate?
+  var reactNativeFactory: RCTReactNativeFactory?
+
+   func application(_ application: UIApplication,didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
     self.moduleName = "ContourAISDK"
     self.dependencyProvider = RCTAppDependencyProvider()
-
-    // You can add your custom initial props in the dictionary below.
-    // They will be passed down to the ViewController used by React Native.
-    self.initialProps = [:]
-
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    let delegate = ReactNativeDelegate()
+    let factory = RCTReactNativeFactory(delegate: delegate)
+    delegate.dependencyProvider = RCTAppDependencyProvider()
+ 
+    reactNativeDelegate = delegate
+    reactNativeFactory = factory
+ 
+    window = UIWindow(frame: UIScreen.main.bounds)
+ 
+    factory.startReactNative(
+      withModuleName: "RnDiffApp",
+      in: window,
+      launchOptions: launchOptions
+    )
+ 
+    return true
   }
+
   override func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
       return ContoursAIFramework.shared.isLandscape ? .landscapeRight : .portrait
   }
@@ -30,4 +46,11 @@ class AppDelegate: RCTAppDelegate {
     Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
   }
+
+  class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
+  override func sourceURL(for bridge: RCTBridge) -> URL? {
+    self.bundleURL()
+  }
 }
+
+
