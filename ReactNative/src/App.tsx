31c6e4fdf33ef66fe48a114c37cc8084f318e37a
Vivek Kumar Sahani <vsahani@uft-int.com>
2025-06-19 16:14:28 +0530

    [CAS-4328] Fixed overlapping issue on Android and iOS

--- ReactNative/src/App.tsx
+++ ReactNative/src/App.tsx
@@ -1,12 +1,15 @@
 
 import React from 'react';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import AppNavigation from './navigation/AppNavigation';
+import { StatusBar } from 'react-native';
 
 export default function App() {
 
   return (
-    <>
+   <SafeAreaView style={{ flex: 1 }} edges={['top', 'bottom']}>
+    <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
       <AppNavigation />
-    </>
+    </SafeAreaView>
   );
 }
